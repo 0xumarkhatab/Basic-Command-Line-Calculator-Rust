9edf94994fb709d513b360937bf57bf8e278d26f
Umar Khatab <umarkhatab467@gmail.com>
2022-06-26 09:46:11 +0500

    Extracting Arguments as String

--- main.rs
+++ main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("Hello, world!"); 
     //  Getting the Command Line Arguments
     let mut arguments: Args=args();
+    //Extracting Arguments as String 
+    let first=arguments.nth(1).unwrap();
+    let op=arguments.nth(0).unwrap().chars().next().unwrap();
+    let second=arguments.nth(0).unwrap();
+     println!("{} {} {} ",first,op,second);
 }
